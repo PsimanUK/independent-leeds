b2514a85a0856d09d389b3db370e3da311f0baa9
j-dublon <j.johnson.dublon@gmail.com>
2020-05-20 17:05:18 +0100

    add basic business registration component

---
+++ src/components/BusinessRegistration.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class BusinessRegistration extends Component {
+  render() {
+    return (
+      <form>
+        <label>Business name:</label>
+        <input />
+        <label>Logo URL:</label>
+        <input />
+        <label>Address:</label>
+        <input />
+        <label>Business type:</label>
+        <input />
+        <label>About:</label>
+        <input />
+        <label>Number of tables available to book:</label>
+        <input />
+      </form>
+    );
+  }
+}
+
+export default BusinessRegistration;
