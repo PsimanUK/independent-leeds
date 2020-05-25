c57393a4c4501a62d81c37926af61eebb5ddf8cd
Simeon Parris <simeonparris@hotmail.com>
2020-05-25 09:53:33 +0100

    remove Map component and references to it in AllBusinesses.

--- src/components/AllBusinesses.jsx
+++ src/components/AllBusinesses.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import Map from './Map';
 import BusinessList from './BusinessList';
 
 const AllBusinesses = () => {
     return (
         <main>
-            <Map />
             <BusinessList />
         </main>
     );
