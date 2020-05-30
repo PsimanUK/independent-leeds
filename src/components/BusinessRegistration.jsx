a6fa8ed6f964f8ac5afc49615223e623bbc6a974
Simeon Parris <simeonparris@hotmail.com>
2020-05-30 09:58:53 +0100

    started moving functionality from BusinessRegCard to BusinessRegistration. Not fully implemented yet.

--- src/components/BusinessRegistration.jsx
+++ src/components/BusinessRegistration.jsx
@@ -98,17 +98,70 @@ class BusinessRegistration extends Component {
       <main>
         {this.state.error && <p>An error has occurred, please try again.</p>}
         {this.state.hasRegistered === false && (
-          <BusinessRegCard handleInput={this.handleInput} handleSubmit={this.handleSubmit} hello="hello" />
-        )}
-        {this.state.hasRegistered === true && (
-          <p>
-            Your registration has been successful. Our admin team will email you
-            once they have verified your business.
-          </p>
-        )}
-      </main>
+          <div >
+            <p>Please enter your details to register a business:</p>
+            <FormControl onSubmit={this.handleSubmit} >
+              <TextField id="outlined-basic" label="Business name" required variant="filled" name="businessName" onChange={this.handleInput} />
+              <TextField id="outlined-basic" label="Email" required variant="filled" textarea="TextareaAutosize" name="businessEmail" onChange={this.handleInput} />
+              <TextField id="outlined-basic" label="Phone number" inputProps={{ pattern: "(0|(+44))(044)?(7|1|2)([0-9]{9})" }} variant="filled" name="phoneNumber" onChange={this.handleInput} />
+              <TextField id="outlined-basic" label="Logo URL" type="url" variant="filled" name="logoUrl" onChange={this.handleInput} />
+              <TextField id="outlined-basic" label="Menu URL" type="url" variant="filled" name="menu" onChange={this.handleInput} />
+              <TextField id="outlined-basic" label="Postcode" textHelper="Without spaces" variant="filled" name="postCode" onChange={this.handleInput} />
+              <TextField id="outlined-basic" label="Address" variant="filled" name="address" onChange={this.handleInput} />
+              <TextField id="outlined-basic" label="Facebook URL" type="url" variant="filled" name="facebook" onChange={this.handleInput} />
+              <TextField id="outlined-basic" label="Twitter URL" type="url" variant="filled" name="twitter" onChange={this.handleInput} />
+              <TextField id="outlined-basic" label="Instagram URL" type="url" variant="filled" name="instagram" onChange={this.handleInput} />
+              <TextField id="outlined-basic" label="Business type" select variant="filled" name="businessType" onChange={this.handleInput} >
+                {businessTypes.map(business => {
+                  return <MenuItem key={business.value} value={business.value}>{business.label}</MenuItem>
+                })}
+              </TextField>
+              <TextField id="outlined-basic" label="Cuisine" select variant="filled" name="cuisine" onChange={√}>
+              {cuisines.map(business => {
+                return <MenuItem key={business.value} value={business.value}>{business.label}</MenuItem>
+              })}
+            </TextField>
+            <TextField
+              id="filled-multiline-flexible"
+              label="Tell us about your business"
+              multiline
+              rows={4}
+              onChange={this.handleInput}
+              variant="filled"
+            />
+            <TextField
+              id="filled-multiline-flexible"
+              label="Tell us your latest updates"
+              multiline
+              rows={4}
+              onChange={this.handleInput}
+              variant="filled"
+            />
+            <FormGroup>
+              <FormLabel>Which diets can you cater for?</FormLabel>
+              <FormControlLabel control={<Checkbox onChange={this.handleInput} name={"vegan"} />} label={"Vegan"} />
+              <FormControlLabel control={<Checkbox onChange={this.handleInput} name={"vegetarian"} />} label={"Vegetarian"} />
+              <FormControlLabel control={<Checkbox onChange={this.handleInput} name={"halal"} />} label={"Halal"} />
+              <FormControlLabel control={<Checkbox onChange={this.handleInput} name={"glutenFree"} />} label={"Gluten free"} />
+            </FormGroup>
+            <Button variant="contained" >Submit</Button>
+          </FormControl>
+        </div >
+    )
+  }
+        {
+  this.state.hasRegistered === true && (
+    <p>
+      Your registration has been successful. Our admin team will email you
+      once they have verified your business.
+    </p>
+  )
+}
+      </main >
     );
   }
 }
 
 export default BusinessRegistration;
+
+          // <BusinessRegCard handleInput={this.handleInput} handleSubmit={this.handleSubmit} hello="hello" />
