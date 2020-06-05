0afac358e3bf45448fb5c297bd679500b9f7ee48
Simeon Parris <simeonparris@hotmail.com>
2020-06-05 10:58:27 +0100

    add Suspense component and .Lazy function to CustomSignIn and CustomSignUp.

--- src/components/AuthWrapper.jsx
+++ src/components/AuthWrapper.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import CustomSignIn from "./CustomSignIn";
+import React, { Component, Suspense } from "react";
+//import CustomSignIn from "./CustomSignIn";
 import InternalApp from "./InternalApp";
 import CustomSignUp from "./CustomSignUp";
 import Title from "./Title";
@@ -14,7 +14,9 @@ class AuthWrapper extends Component {
   };
 
   render() {
-    console.log(this.props.authState, "<-- authState in AuthWrapper");
+
+    const CustomSignIn = React.lazy(() => import("./CustomSignIn"));
+    const CustomSignUp = React.lazy(() => import("./CustomSignUp"));
     return (
       <div className="authWrapper">
         {this.props.authState !== "signedIn" ? (
@@ -22,21 +24,24 @@ class AuthWrapper extends Component {
         ) : (
             <NavBar loggedInUser={this.state.username} />
           )}
-        <Router>
-          <CustomSignIn
-            path="/"
-            authState={this.props.authState}
-            updateUsername={this.updateUsername}
-            onStateChange={this.props.onStateChange}
-            className="landingPage"
-          />
-          <CustomSignUp
-            path="/register"
-            authState={this.props.authState}
-            onStateChange={this.props.onStateChange}
-            className="landingPage"
-          />
-        </Router>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Router>
+            <CustomSignIn
+              path="/"
+              authState={this.props.authState}
+              updateUsername={this.updateUsername}
+              onStateChange={this.props.onStateChange}
+              className="landingPage"
+            />
+
+            <CustomSignUp
+              path="/register"
+              authState={this.props.authState}
+              onStateChange={this.props.onStateChange}
+              className="landingPage"
+            />
+          </Router>
+        </Suspense>
         <InternalApp
           authState={this.props.authState}
           onStateChange={this.props.onStateChange}
